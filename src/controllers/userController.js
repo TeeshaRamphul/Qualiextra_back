27131c9614a4b9c59e9d29fe6509de1c9e7e1222
TeeshaRamphul <teesha.ramphul@oclock.school>
2025-05-01 01:27:22 +0200

    feat: implement user registration with password hashing and validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,61 @@
+import PasswordValidator from "password-validator";
+import argon2 from "argon2";
 import { User } from "../models/User.js";
+
 
 const userController = {
 
-    async createUser(req, res) {
-        // Enregistrer la nouvelle liste en DB
-        const user = await User.create(req.body);
-        
-        if (!user){
-            return next(error);
+    async registerUser(req, res) {
+        // Récupérer les données du body
+        const { firstname, lastname, email, password, role } = req.body;
+
+        // Vérifier que tous les champs sont présents
+        if (!firstname || !lastname || !email || !password) {
+            return res.status(400).json({ error: 'Tous les champs (firstname, lastname, email, password) sont obligatoires.' });
         }
-        res.status(201).json(user);
+
+        // Vérifier que le mot de passe est suffisamment complexe
+        const schema = new PasswordValidator()
+        .is().min(12)                  // 12 caractères mini
+        .is().max(100)                 // Maximum 100
+        .has().uppercase()             // 1 majuscule
+        .has().lowercase()             // 1 minuscule
+        .has().digits(1)               // 1 chiffre
+        .has().symbols(1)              // 1 symbole
+        .has().not().spaces();         // Pas d'espace
+      
+        if (! schema.validate(password)) {
+            return res.status(400).json({ error: "Le mot de passe n'est pas suffisamment complexe. Veuillez utiliser au moins 12 caractères, une majuscule, une minuscule, un chiffre et un symbole." });
+        }
+    
+        // Vérifier si un utilisateur avec le même email n'existe pas déjà en BDD => faire une requête pour récupérer un utilisateur par son email
+        const existing = await User.findOne({ where: { email: email }});
+        if (existing) {
+            return res.status(409).json({ error: "L'email renseigné est déjà utilisé." });
+        }
+
+        // Vérifier le format de l'email --> on pourrait envoyer un mail de validation
+
+        
+        // Hacher le mot de passe (pour ne pas le sauvegarder en clair)
+        const hash = await argon2.hash(password);
+    
+        // Sauvegarder l'utilisateur en BDD (via le model User)
+        try {
+            await User.create({
+                firstname,
+                lastname,
+                email,
+                password: hash,
+                role: role || 'member',
+            });
+    
+            return res.status(201).json({ successMessage: "Veuillez à présent vous authentifier." });
+    
+        } catch (error) {
+            console.error('Erreur lors de l\'enregistrement de l\'utilisateur :', error); // LOG l'erreur SQL exacte !
+            return res.status(500).json({ error: "Erreur lors de l'enregistrement de l'utilisateur." });
+        }
     },
 
     async getAllUsers(req, res) {
--- src/router.js
+++ src/router.js
@@ -3,10 +3,10 @@ import { userController }  from './controllers/userController.js'
 
 const router = Router();
 
+router.post("/users", userController.registerUser);
 
 router.get("/users", userController.getAllUsers);
 router.get("/users/:id", userController.getOneUser);
-router.post("/users", userController.createUser);
 router.patch("/users/:id", userController.updateUser);
 router.delete("/users/:id", userController.deleteUser);
 
